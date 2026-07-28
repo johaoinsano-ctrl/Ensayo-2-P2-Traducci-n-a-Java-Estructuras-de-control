6ac11a086ff05cf233bafc0f98f37ff73ce20a80
johaoinsano-ctrl <johaoinsano@gmail.com>
2026-07-27 19:11:27 -0500

    Update Ejercicio10EstrucControl.java

--- Ejercicio10EstrucControl.java
+++ Ejercicio10EstrucControl.java
@@ -2,33 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.ejercicio10estruccontrol;
-
 import java.util.Scanner;
 
 /**
  *
  * @author Johao Smith Urbina González
  */
-public class Ejercicio10EstrucControl {
+public class Main {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
- 
+
         int suma = 0;
         int cantidad = 10;
- 
+
         for (int i = 1; i <= cantidad; i++) {
             System.out.print("Ingrese el numero " + i + ": ");
             int numero = Integer.parseInt(teclado.nextLine());
             suma += numero;
         }
- 
+
         double promedio = (double) suma / cantidad;
- 
+
         System.out.println("\n--- Resultados ---");
         System.out.println("Suma: " + suma);
         System.out.println("Promedio: " + promedio);
     }
 }
- 
