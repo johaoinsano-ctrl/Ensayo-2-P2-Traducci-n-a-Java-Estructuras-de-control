17d04b986de6af93c5782510acd75651a3ded8c2
johaoinsano-ctrl <johaoinsano@gmail.com>
2026-07-27 19:10:11 -0500

    Update Ejercicio1EstrucControl1.java

--- Ejercicio1EstrucControl1.java
+++ Ejercicio1EstrucControl1.java
@@ -2,31 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.ejercicio1estruccontrol1;
-
 import java.util.Scanner;
 
 /**
  *
  * @author Johao Smith Urbina González
  */
-public class Ejercicio1EstrucControl1 {
+public class Main {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
- 
+
         System.out.print("Ingrese su nombre: ");
         String nombre = teclado.nextLine();
- 
+
         System.out.print("Ingrese su edad: ");
         int edad = Integer.parseInt(teclado.nextLine());
- 
+
         System.out.print("Ingrese su carrera: ");
         String carrera = teclado.nextLine();
- 
+
         System.out.print("Ingrese su estatura (m): ");
         double estatura = Double.parseDouble(teclado.nextLine());
- 
+
         System.out.println("\n--- Datos ingresados ---");
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
@@ -34,5 +32,3 @@ public class Ejercicio1EstrucControl1 {
         System.out.println("Estatura: " + estatura + " m");
     }
 }
-     
-
