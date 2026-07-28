4ee298041fc3f895cf06a2f62c4ffb6ca7a0e23d
johaoinsano-ctrl <johaoinsano@gmail.com>
2026-07-27 19:05:38 -0500

    Update Ejercicio8EstrucControl.java

--- Ejercicio8EstrucControl.java
+++ Ejercicio8EstrucControl.java
@@ -2,26 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.ejercicio8estruccontrol;
-
 import java.util.Scanner;
 
 /**
  *
  * @author Johao Smith Urbina González
  */
-public class Ejercicio8EstrucControl {
+public class Main {
 
      public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         String contrasenaCorrecta = "java2026";
         String contrasenaIngresada;
- 
+
         do {
             System.out.print("Ingrese la contrasena: ");
             contrasenaIngresada = teclado.nextLine();
+
         } while (!contrasenaIngresada.equals(contrasenaCorrecta));
- 
+
         System.out.println("Acceso concedido.");
     }
 }
