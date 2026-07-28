beef898441a1d5fdc8cc5ea88a4d267c293afa2a
johaoinsano-ctrl <johaoinsano@gmail.com>
2026-07-27 19:06:21 -0500

    Update Ejercicio7EstrucControl.java

--- Ejercicio7EstrucControl.java
+++ Ejercicio7EstrucControl.java
@@ -2,35 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.ejercicio7estruccontrol;
-
 import java.util.Scanner;
 
 /**
  *
  * @author Johao Smith Urbina González
  */
-public class Ejercicio7EstrucControl {
+public class Main {
 
     public static void main(String[] args) {
-      Scanner teclado = new Scanner(System.in);
- 
+        Scanner teclado = new Scanner(System.in);
+
         int numero;
         int cantidad = 0;
         int sumaTotal = 0;
- 
+
         System.out.println("Ingrese numeros positivos. Ingrese un numero negativo para terminar.");
- 
+
         do {
             System.out.print("Numero: ");
             numero = Integer.parseInt(teclado.nextLine());
- 
+
             if (numero >= 0) {
                 sumaTotal += numero;
                 cantidad++;
             }
+
         } while (numero >= 0);
- 
+
         System.out.println("\n--- Resultados ---");
         System.out.println("Cantidad de numeros ingresados: " + cantidad);
         System.out.println("Suma total: " + sumaTotal);
