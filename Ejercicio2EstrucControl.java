af537a2311d9224d6f3de60d6a977b9874d178e4
johaoinsano-ctrl <johaoinsano@gmail.com>
2026-07-27 19:09:33 -0500

    Update Ejercicio2EstrucControl.java

--- Ejercicio2EstrucControl.java
+++ Ejercicio2EstrucControl.java
@@ -2,31 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.ejercio2estruccontrol;
-
 import java.util.Scanner;
 
 /**
  *
  * @author Johao Smith Urbina González
  */
-public class Ejercio2EstrucControl {
+public class Main {
 
     public static void main(String[] args) {
-    Scanner teclado = new Scanner(System.in);
- 
+        Scanner teclado = new Scanner(System.in);
+
         System.out.print("Ingrese el primer numero entero: ");
         int numero1 = Integer.parseInt(teclado.nextLine());
- 
+
         System.out.print("Ingrese el segundo numero entero: ");
         int numero2 = Integer.parseInt(teclado.nextLine());
- 
+
         int suma = numero1 + numero2;
         int resta = numero1 - numero2;
         int multiplicacion = numero1 * numero2;
         double division = (double) numero1 / numero2;
         int residuo = numero1 % numero2;
- 
+
         System.out.println("\n--- Resultados ---");
         System.out.println("Suma: " + suma);
         System.out.println("Resta: " + resta);
@@ -35,4 +33,3 @@ public class Ejercio2EstrucControl {
         System.out.println("Residuo: " + residuo);
     }
 }
-
